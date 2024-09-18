03b6991b6dea5deccaa89810a86178647604959a
mzulseptriawan <mzulfikarseptriawan@gmail.com>
2024-09-18 21:53:52 +0700

    added api for get data Profile

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\ProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class, 'login']);
+
+Route::apiResource('/profiles',ProfileController::class);
